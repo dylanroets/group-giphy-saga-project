bc1946f8d5697fbb6dbfd8bb5df7009fd34617dc
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-08 09:44:51 -0600

    Components created for Home and Fav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Favorites from '../Favorites/Favorites';
+import Home from '../Home/Home';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <Home/>
+      <Favorites/>
     </div>
   );
 }
---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,11 @@
+
+
+function Favorites(){
+    return(
+        <>
+            <h1>I am Favorites</h1>
+        </>
+    )
+}
+
+export default Favorites;
