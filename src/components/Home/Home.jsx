8f6d70cfd6e6bbd2760b37f852bf09e17826e7c6
Dylan Roets <dylanroets@yahoo.com>
2022-12-09 11:45:54 -0600

    finished things

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,19 @@ function Home(){
     const dispatch = useDispatch();
     const searchArray = useSelector(store => store.searchReducer);
 
+    const [category, setNewCategory]= useState('');
     const [newSearch, setNewSearch] = useState('');
+
+    const setCategory = (event) => {
+        setNewCategory(event.target.value);
+    }
+
+    const handleFavorite = (image) => {
+        dispatch ({
+            type: 'ADD_TO_FAVORITES',
+            payload: {category_id: category, URL: image.embed_url, description: image.title}
+        });
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -37,6 +49,15 @@ function Home(){
                     {return (
                         <div className="grid-item" key={image.id}>
                             <iframe src={image.embed_url} height='200px' width='200px'/>
+                            <button data-id={image.id} onClick={() => handleFavorite(image)}>Favorite</button>
+                            <select name="categories" id="categories" onChange={(event) => setCategory(event)}>
+                                <option value=''></option>
+                                <option value="1">Funny</option>
+                                <option value="2">Cohort</option>
+                                <option value="3">Cartoon</option>
+                                <option value="4">NSFW</option>
+                                <option value="5">Meme</option>
+                            </select>
                         </div>
                     )}
                 )}
